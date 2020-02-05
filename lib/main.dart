2a7a119730e4d12742f99b6f4969fb1ab59b6217
dabitdev <imdavidfernandez@gmail.com>
2020-02-05 12:18:53 -0500

    qr code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:qrscan/qrscan.dart' as scanner;
 
 void main() => runApp(MyApp());
+
+var myController = TextEditingController();
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -46,50 +49,39 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Container(
         padding: new EdgeInsets.all(25.0),
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
               'Please import the BitCoin wallet adding or scanning the secret seed key',
             ),
             Row(
-              children:<Widget>[
-                Expanded(child: TextField()),
-                SizedBox(width:70, child:IconButton(icon:Icon(Icons.camera_alt), onPressed: (){})),
+              children: <Widget>[
+                Expanded(child: TextField(controller: myController,)),
+                SizedBox(
+                    width: 70,
+                    child: IconButton(
+                        icon: Icon(Icons.camera_alt),
+                        onPressed: () async {
+                          String cameraScanResult = await scanner.scan();
+                          print(cameraScanResult);
+                          myController.text = cameraScanResult;
+                        })),
               ],
             ),
-            Container(margin: new EdgeInsets.all(25.0), child: RaisedButton(textColor:Colors.white, color:Colors.blue, onPressed: (){}, 
-              child: Text("Import", style: TextStyle(fontSize: 20.0))))
+            Container(
+                margin: new EdgeInsets.all(25.0),
+                child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: () {},
+                    child: Text("Import", style: TextStyle(fontSize: 20.0))))
           ],
         ),
       ),
