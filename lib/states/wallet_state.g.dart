68098a7bb658da6f32b811c050c1afeaee1d89ac
dabitdev <imdavidfernandez@gmail.com>
2020-08-29 22:02:25 -0400

    updating mobx and mobx_gen

--- lib/states/wallet_state.g.dart
+++ lib/states/wallet_state.g.dart
@@ -6,35 +6,41 @@ part of 'wallet_state.dart';
 // StoreGenerator
 // **************************************************************************
 
-// ignore_for_file: non_constant_identifier_names, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$WalletState on _WalletState, Store {
   final _$balanceAtom = Atom(name: '_WalletState.balance');
 
   @override
   int get balance {
-    _$balanceAtom.context.enforceReadPolicy(_$balanceAtom);
-    _$balanceAtom.reportObserved();
+    _$balanceAtom.reportRead();
     return super.balance;
   }
 
   @override
   set balance(int value) {
-    _$balanceAtom.context.conditionallyRunInAction(() {
+    _$balanceAtom.reportWrite(value, super.balance, () {
       super.balance = value;
-      _$balanceAtom.reportChanged();
-    }, _$balanceAtom, name: '${_$balanceAtom.name}_set');
+    });
   }
 
   final _$_WalletStateActionController = ActionController(name: '_WalletState');
 
   @override
   void setBalance(int newBalance) {
-    final _$actionInfo = _$_WalletStateActionController.startAction();
+    final _$actionInfo = _$_WalletStateActionController.startAction(
+        name: '_WalletState.setBalance');
     try {
       return super.setBalance(newBalance);
     } finally {
       _$_WalletStateActionController.endAction(_$actionInfo);
     }
+  }
+
+  @override
+  String toString() {
+    return '''
+balance: ${balance}
+    ''';
   }
 }
